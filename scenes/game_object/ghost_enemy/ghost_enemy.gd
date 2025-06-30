05ec7afa5fdc1d200198bb9c352bb127078ebb72
dweckerly <d.weckerly@gmail.com>
2025-06-30 09:10:34 -0500

    new enemy

--- scenes/game_object/ghost_enemy/ghost_enemy.gd
+++ scenes/game_object/ghost_enemy/ghost_enemy.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
+@onready var velocity_component: VelocityComponent = $VelocityComponent
 @onready var visuals: Node2D = $Visuals
-@onready var velocity_component: VelocityComponent = $VelocityComponent 
-
 
 func _process(delta: float) -> void:
 	velocity_component.accelerate_to_player()
@@ -11,3 +10,4 @@ func _process(delta: float) -> void:
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(move_sign, 1)
+	
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -2,14 +2,16 @@ extends Node
 
 const SPAWN_RADIUS: float = 350 # half of viewport width +16 pixels
 
-@export var basic_enemy_scene: PackedScene
+@export var enemy_scenes: Array[PackedScene]
 @export var arena_time_manager: ArenaTimeManager
 
 @onready var timer = $Timer
 
 var base_spawn_time = 0
+var enemy_table: WeightedTable = WeightedTable.new()
 
 func _ready() -> void:
+	enemy_table.add_item(enemy_scenes[0], 10)
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
@@ -39,12 +41,16 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
-	var enemy = basic_enemy_scene.instantiate() as Node2D
+	var enemy_scene = enemy_table.pick_item()
+	var enemy = enemy_scene.instantiate() as Node2D
+	
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = get_spawn_position()
 
 func on_arena_difficulty_increased(arean_difficulty: int):
 	var time_off = min((0.1 / 12) * arean_difficulty, 0.7)
-	print(time_off)
 	timer.wait_time = base_spawn_time - time_off
+	
+	if arean_difficulty == 6:
+		enemy_table.add_item(enemy_scenes[1], 20)
