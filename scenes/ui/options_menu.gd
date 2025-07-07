02abdef2f7ddb42bb374ec789c25f525d483fe1a
dweckerly <d.weckerly@gmail.com>
2025-07-07 10:10:39 -0500

    menus

---
+++ scenes/ui/options_menu.gd
@@ -0,0 +1,55 @@
+extends CanvasLayer
+
+signal back_pressed
+
+@onready var window_button: Button = %WindowButton
+@onready var music_slider: HSlider = %MusicSlider
+@onready var sfx_slider: HSlider = %SFXSlider
+@onready var back_button: Button = %BackButton
+
+
+func _ready() -> void:
+	window_button.pressed.connect(on_window_button_pressed)
+	back_button.pressed.connect(on_back_pressed)
+	music_slider.value_changed.connect(on_audio_slider_changed.bind("music"))
+	sfx_slider.value_changed.connect(on_audio_slider_changed.bind("sfx"))
+	update_display()
+	
+
+func update_display() -> void:
+	window_button.text = "Windowed"
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		window_button.text = "Full Screen"
+	
+	music_slider.value = get_bus_volume_percent("music")
+	sfx_slider.value = get_bus_volume_percent("sfx")
+
+
+func get_bus_volume_percent(bus_name: String) -> float:
+	var bus_index: int = AudioServer.get_bus_index(bus_name)
+	var volume_db: float =  AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(volume_db)
+
+
+func set_bus_volume_percent(bus_name: String, percent: float) -> void:
+	var bus_index: int = AudioServer.get_bus_index(bus_name)
+	var volume_db = linear_to_db(percent)
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+
+func on_audio_slider_changed(value: float, bus_name: String) -> void:
+	set_bus_volume_percent(bus_name, value)
+	update_display()
+
+
+func on_window_button_pressed() -> void:
+	var mode  = DisplayServer.window_get_mode()
+	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	update_display()
+
+
+func on_back_pressed() -> void:
+	back_pressed.emit()
