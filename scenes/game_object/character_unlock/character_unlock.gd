1fef0c67eefd0cc882e42a7ba78d923f196807cb
dweckerly <d.weckerly@gmail.com>
2025-09-11 12:42:50 -0500

    added unlock screen to character unlock object

--- scenes/game_object/character_unlock/character_unlock.gd
+++ scenes/game_object/character_unlock/character_unlock.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@export var character_id: String
+@export var character: Character
+@export var unlock_screen: PackedScene
 
 @onready var area_2d: Area2D = $Area2D
 
@@ -9,5 +10,8 @@ func _ready() -> void:
 	
 
 func on_area_entered(area2d: Area2D) -> void:
-	MetaProgression.unlock_character(character_id)
+	MetaProgression.unlock_character(character.id)
+	var cu_screen = unlock_screen.instantiate()
+	get_tree().root.add_child(cu_screen)
+	cu_screen.unlock_animation(character)
 	queue_free()
--- scenes/ui/character_unlock_screen.gd
+++ scenes/ui/character_unlock_screen.gd
@@ -1,6 +1,17 @@
 extends CanvasLayer
 
-@onready var texture_rect: TextureRect = $MarginContainer/TextureRect
+@onready var texture_rect: TextureRect = $TextureRect
+@onready var label: Label = $Label
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-	pass
+	get_tree().paused = true
+	animation_player.play("in")
+
+func unlock_animation(character: Character) -> void:
+	texture_rect.texture = character.sprite
+	label.text = "\n\n" + character.display_name + " UNLOCKED!"
+	animation_player.play("character_in")
+	await get_tree().create_timer(2).timeout
+	get_tree().paused = false
+	queue_free()
