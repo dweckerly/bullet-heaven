ab086f8d5578c44b857e41a547ab40851f0c0aa0
dweckerly <d.weckerly@gmail.com>
2025-09-13 13:55:32 -0500

    change character unlock object if character is already unlocked

--- scenes/game_object/character_unlock/character_unlock.gd
+++ scenes/game_object/character_unlock/character_unlock.gd
@@ -3,15 +3,33 @@ extends Node2D
 @export var character: Character
 @export var unlock_screen: PackedScene
 
+@onready var blood_particles: GPUParticles2D = $Visuals/BloodParticles
+@onready var gold_particles: GPUParticles2D = $Visuals/GoldParticles
+@onready var sprite_2d: Sprite2D = $Visuals/Sprite2D
 @onready var area_2d: Area2D = $Area2D
 
-func _ready() -> void:
-	area_2d.area_entered.connect(on_area_entered)
-	
+const HEART = preload("res://assets/objects/heart.png")
+var locked: bool = true 
 
-func on_area_entered(area2d: Area2D) -> void:
+func _ready() -> void:
+	locked = MetaProgression.save_data['characters'][character.id]['locked']
+	if locked:
+		area_2d.area_entered.connect(on_area_entered_locked)
+	else:
+		sprite_2d.texture = HEART
+		blood_particles.emitting = false
+		area_2d.area_entered.connect(on_area_entered_unlocked)
+
+func on_area_entered_locked(area2d: Area2D) -> void:
 	MetaProgression.unlock_character(character.id)
 	var cu_screen = unlock_screen.instantiate()
 	get_tree().root.add_child(cu_screen)
 	cu_screen.unlock_animation(character)
+	queue_free()
+
+
+func on_area_entered_unlocked(area2d: Area2D) -> void:
+	sprite_2d.visible = false
+	gold_particles.emitting = true
+	await get_tree().create_timer(5).timeout
 	queue_free()
